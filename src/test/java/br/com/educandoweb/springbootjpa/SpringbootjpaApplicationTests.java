6fd00bc35b953deaf5fbcdf5d2c96e120bd36749
thiago.sano <thiago.sano@outlook.com>
2025-03-18 11:22:35 -0300

    Project created

---
+++ src/test/java/br/com/educandoweb/springbootjpa/SpringbootjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.educandoweb.springbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
