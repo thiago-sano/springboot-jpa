a9532f712fc87bebe5c371291206b46c802541cd
thiago.sano <thiago.sano@outlook.com>
2025-03-24 10:40:19 -0300

    feat: Exception handling - findById

---
+++ src/main/java/br/com/educandoweb/springbootjpa/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package br.com.educandoweb.springbootjpa.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
